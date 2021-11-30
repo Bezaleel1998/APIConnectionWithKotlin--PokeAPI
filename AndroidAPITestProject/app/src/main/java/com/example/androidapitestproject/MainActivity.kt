5f46080f823903e18b1d4ad7637714468bbab113
NightHawk1998 <bezaleelairbornedivision@yahoo.com>
2021-11-30 14:12:09 +0700

    adding something that i forget

--- AndroidAPITestProject/app/src/main/java/com/example/androidapitestproject/MainActivity.kt
+++ AndroidAPITestProject/app/src/main/java/com/example/androidapitestproject/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.androidapitestproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toolbar
 
 
 
@@ -12,16 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /*// assigning ID of the toolbar to a variable
-        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
-        toolbar?.title = "POKEMON LIST"
-
-        // using toolbar as ActionBar
-        //setSupportActionBar(toolbar)
-        setActionBar(toolbar)*/
-
-
 
     }
 
